e34659f5c86da23e0cbbe858ef841d849d9a252c
vinaykumar2318 <vinayku.1814@gmail.com>
2024-12-23 01:16:26 +0530

    resolved a time related problem

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,18 +1,34 @@
+'use client'
+
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useGetCalls } from '@/hooks/useGetCalls';
 import React from 'react'
 
 const Page = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Kolkata' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeZone: 'Asia/Kolkata' })).format(now);
+
+  
+  const { upcomingCalls } = useGetCalls();
+  const call = upcomingCalls.at(-1);
+  let timeMeet;
+  if(call){
+    timeMeet = call['state'].startsAt?.toLocaleTimeString('en-IN', { 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      hour12: true 
+    });
+    console.log(time);
+  }
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming meeting at 2:00 P.M.
+            Upcoming meeting at {timeMeet}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
