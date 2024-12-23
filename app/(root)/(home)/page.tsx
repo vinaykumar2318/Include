9aed6dd99951eb6c47476759eea42506973d0bf2
vinaykumar2318 <vinayku.1814@gmail.com>
2024-12-23 14:24:22 +0530

    Improved home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,22 +13,27 @@ const Page = () => {
   
   const { upcomingCalls } = useGetCalls();
   const call = upcomingCalls.at(-1);
-  let timeMeet;
+  let timeMeet, dateMeet;
   if(call){
+    const startDate = call['state'].startsAt;
+
     timeMeet = call['state'].startsAt?.toLocaleTimeString('en-IN', { 
       hour: '2-digit', 
       minute: '2-digit', 
       hour12: true 
     });
-    console.log(time);
+
+    dateMeet = new Intl.DateTimeFormat('en-IN', { 
+      dateStyle: 'full' 
+    }).format(startDate);
   }
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming meeting at {timeMeet}
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 p-11">
+          <h2 className="glassmorphism max-w-[273px] rounded-lg py-2 text-center text-base font-normal">
+            Upcoming meeting at {timeMeet} on {dateMeet}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
